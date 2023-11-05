7aa217ba007f720f7cb9f1875fa9404297a82022
thanhlemm <109269027+thanhlemm@users.noreply.github.com>
2023-11-05 14:55:10 +0700

    hi 4

--- saleappv2/app/__init__.py
+++ saleappv2/app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from urllib.parse import quote
 from flask_sqlalchemy import SQLAlchemy
 
-#hi 3
+#hi 4
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/flashsaledb?charset=utf8mb4" % quote('t-TThanhlemmilkk021203')
